33a5aaa4413df7dc09cdd60a3388a0647b26f971
Māris Krivtežs <marisks@gmail.com>
2021-02-24 15:36:12 +0200

    Updated packaging.

--- pack.ps1
+++ pack.ps1
@@ -1,5 +1,3 @@
 $outputDir = ".\package\"
-$build = "Release"
-$version = "1.0.0"
 
-.\.nuget\nuget.exe pack ".\src\Geta.NotFoundHandler\Geta.NotFoundHandler.csproj" -IncludeReferencedProjects -properties Configuration=$build -Version $version -OutputDirectory $outputDir
+dotnet pack --output $outputDir
