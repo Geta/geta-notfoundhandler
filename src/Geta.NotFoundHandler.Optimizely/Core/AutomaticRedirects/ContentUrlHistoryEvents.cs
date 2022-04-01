584d10215e43bffe48c66e82c7fe50c887f89b0b
Māris Krivtežs <marisks@gmail.com>
2022-04-01 12:36:35 +0300

    Automatic Redirects - Added mandatory, but pointless header.

--- src/Geta.NotFoundHandler.Optimizely/Core/AutomaticRedirects/ContentUrlHistoryEvents.cs
+++ src/Geta.NotFoundHandler.Optimizely/Core/AutomaticRedirects/ContentUrlHistoryEvents.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Geta Digital. All rights reserved.
+// Licensed under Apache-2.0. See the LICENSE file in the project root for more information
+
 using EPiServer;
 using EPiServer.Core;
 using Geta.NotFoundHandler.Optimizely.Infrastructure.Configuration;
