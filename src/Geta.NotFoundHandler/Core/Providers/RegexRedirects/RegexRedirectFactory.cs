f296fbc3958c165e4c2a9a7fcf9dfed8177fa081
Māris Krivtežs <marisks@gmail.com>
2022-09-02 15:15:10 +0300

    Created regex redirect repository.

---
+++ src/Geta.NotFoundHandler/Core/Providers/RegexRedirects/RegexRedirectFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Geta.NotFoundHandler.Core.Providers.RegexRedirects;
+
+public class RegexRedirectFactory
+{
+    private const int Timeout = 100; // TODO: Read from configuration
+
+    public virtual RegexRedirect Create(Guid id, string oldUrlRegex, string newUrlFormat, int orderNumber, int timeoutCount)
+    {
+        return new RegexRedirect(id,
+                                 new Regex(oldUrlRegex, RegexOptions.Compiled, TimeSpan.FromMilliseconds(Timeout)),
+                                 newUrlFormat,
+                                 orderNumber,
+                                 timeoutCount);
+    }
+}
---
+++ src/Geta.NotFoundHandler/Data/SqlRegexRedirectRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Geta.NotFoundHandler.Core.Providers.RegexRedirects;
+
+namespace Geta.NotFoundHandler.Data;
+
+public class SqlRegexRedirectRepository : IRegexRedirectLoader
+{
+    private const string RegexRedirectsTable = "[dbo].[NotFoundHandler.RegexRedirects]";
+
+    private const string AllFields = "Id, OldUrlRegex, NewUrlFormat, OrderNumber, TimeoutCount";
+    
+    private readonly IDataExecutor _dataExecutor;
+    private readonly RegexRedirectFactory _regexRedirectFactory;
+
+    public SqlRegexRedirectRepository(IDataExecutor dataExecutor, RegexRedirectFactory regexRedirectFactory)
+    {
+        _dataExecutor = dataExecutor;
+        _regexRedirectFactory = regexRedirectFactory;
+    }
+
+    public IEnumerable<RegexRedirect> GetAll()
+    {
+        var sqlCommand = $@"SELECT {AllFields} FROM {RegexRedirectsTable}";
+
+        var dataTable = _dataExecutor.ExecuteQuery(sqlCommand);
+
+        return ToRedirects(dataTable);
+    }
+
+    private IEnumerable<RegexRedirect> ToRedirects(DataTable table)
+    {
+        return table.AsEnumerable().Select(ToRedirect);
+    }
+
+    private RegexRedirect ToRedirect(DataRow x)
+    {
+        return _regexRedirectFactory.Create(
+            x.Field<Guid>("Id"),
+            x.Field<string>("OldUrlRegex"),
+            x.Field<string>("NewUrlFormat"),
+            x.Field<int>("OrderNumber"),
+            x.Field<int>("TimeoutCount"));
+
+    }
+}
