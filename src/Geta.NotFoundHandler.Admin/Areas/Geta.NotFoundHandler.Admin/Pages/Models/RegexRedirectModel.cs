4a755a3c9c6037aa8ffb1e5bf197dc478877b824
Māris Krivtežs <marisks@gmail.com>
2022-09-07 13:47:21 +0300

    Added validation.

--- src/Geta.NotFoundHandler.Admin/Areas/Geta.NotFoundHandler.Admin/Pages/Models/RegexRedirectModel.cs
+++ src/Geta.NotFoundHandler.Admin/Areas/Geta.NotFoundHandler.Admin/Pages/Models/RegexRedirectModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 
 namespace Geta.NotFoundHandler.Admin.Pages.Geta.NotFoundHandler.Admin.Models;
@@ -6,7 +7,13 @@ namespace Geta.NotFoundHandler.Admin.Pages.Geta.NotFoundHandler.Admin.Models;
 public class RegexRedirectModel
 {
     public Guid? Id { get; set; }
+    [Required]
+    [Display(Name = "Old URL Regex")]
     public string OldUrlRegex { get; set; }
+    [Required]
+    [Display(Name = "New URL Format")]
     public string NewUrlFormat { get; set; }
+    [Required]
+    [Display(Name = "Order Number")]
     public int OrderNumber { get; set; }
 }
--- src/Geta.NotFoundHandler.Admin/Areas/Geta.NotFoundHandler.Admin/Pages/Regex.cshtml.cs
+++ src/Geta.NotFoundHandler.Admin/Areas/Geta.NotFoundHandler.Admin/Pages/Regex.cshtml.cs
@@ -7,105 +7,105 @@ using Geta.NotFoundHandler.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Geta.NotFoundHandler.Admin.Areas.Geta.NotFoundHandler.Admin
+namespace Geta.NotFoundHandler.Admin.Areas.Geta.NotFoundHandler.Admin;
+
+public class RegexModel : PageModel
 {
-    public class RegexModel : PageModel
+    private readonly IRegexRedirectLoader _redirectLoader;
+    private readonly IRegexRedirectsService _regexRedirectsService;
+
+    public RegexModel(
+        IRegexRedirectsService regexRedirectsService,
+        IRegexRedirectLoader redirectLoader)
     {
-        private readonly IRegexRedirectsService _regexRedirectsService;
-        private readonly IRegexRedirectLoader _redirectLoader;
+        _regexRedirectsService = regexRedirectsService;
+        _redirectLoader = redirectLoader;
+    }
 
-        public RegexModel(
-            IRegexRedirectsService regexRedirectsService,
-            IRegexRedirectLoader redirectLoader)
-        {
-            _regexRedirectsService = regexRedirectsService;
-            _redirectLoader = redirectLoader;
-        }
+    public string Message { get; set; }
 
-        public string Message { get; set; }
+    public IEnumerable<RegexRedirect> Items { get; set; } = Enumerable.Empty<RegexRedirect>();
 
-        public IEnumerable<RegexRedirect> Items { get; set; } = Enumerable.Empty<RegexRedirect>();
+    private string EditItemId { get; set; }
 
-        private string EditItemId { get; set; }
+    [BindProperty]
+    public RegexRedirectModel RegexRedirect { get; set; }
 
-        [BindProperty]
-        public RegexRedirectModel RegexRedirect { get; set; }
+    public void OnGet()
+    {
+        Load();
+    }
 
-        public void OnGet()
+    public IActionResult OnPostCreate()
+    {
+        if (!ModelState.IsValid)
         {
             Load();
-        }
-
-        public IActionResult OnPostCreate()
-        {
-            if (!ModelState.IsValid)
-            {
-                Load();
-                return Page();
-            }
-
-            _regexRedirectsService.Create(RegexRedirect.OldUrlRegex, RegexRedirect.NewUrlFormat, RegexRedirect.OrderNumber);
-
-            return RedirectToPage();
-        }
-
-        public IActionResult OnPostDelete(Guid id)
-        {
-            _regexRedirectsService.Delete(id);
-
-            return RedirectToPage();
-        }
-
-        public IActionResult OnPostEdit(Guid id)
-        {
-            ModelState.Clear();
-
-            Load();
-
-            EditItemId = id.ToString();
-
-            var redirect = _redirectLoader.Get(id);
-
-            RegexRedirect = new RegexRedirectModel
-            {
-                Id = redirect.Id,
-                OldUrlRegex = redirect.OldUrlRegex.ToString(),
-                NewUrlFormat = redirect.NewUrlFormat,
-                OrderNumber = redirect.OrderNumber
-            };
-
             return Page();
         }
 
-        public IActionResult OnPostUpdate(Guid id)
-        {
-            _regexRedirectsService.Update(id,
-                                          RegexRedirect.OldUrlRegex,
-                                          RegexRedirect.NewUrlFormat,
-                                          RegexRedirect.OrderNumber);
-            return RedirectToPage();
-        }
+        _regexRedirectsService.Create(RegexRedirect.OldUrlRegex, RegexRedirect.NewUrlFormat, RegexRedirect.OrderNumber);
 
-        private void Load()
-        {
-            var items = FindRedirects();
-            Message = $"There are currently stored {items.Count()} Regex redirects.";
-            Items = items;
-        }
+        return RedirectToPage();
+    }
 
-        private IList<RegexRedirect> FindRedirects()
-        {
-            return _redirectLoader.GetAll().ToList();
-        }
+    public IActionResult OnPostDelete(Guid id)
+    {
+        _regexRedirectsService.Delete(id);
 
-        public bool IsEditing(Guid? id)
-        {
-            return id.ToString() == EditItemId;
-        }
+        return RedirectToPage();
+    }
 
-        public bool IsEditing()
+    public IActionResult OnPostEdit(Guid id)
+    {
+        ModelState.Clear();
+
+        Load();
+
+        EditItemId = id.ToString();
+
+        var redirect = _redirectLoader.Get(id);
+
+        RegexRedirect = new RegexRedirectModel
         {
-            return !string.IsNullOrEmpty(EditItemId);
-        }
+            Id = redirect.Id,
+            OldUrlRegex = redirect.OldUrlRegex.ToString(),
+            NewUrlFormat = redirect.NewUrlFormat,
+            OrderNumber = redirect.OrderNumber
+        };
+
+        return Page();
+    }
+
+    public IActionResult OnPostUpdate(Guid id)
+    {
+        _regexRedirectsService.Update(id,
+                                      RegexRedirect.OldUrlRegex,
+                                      RegexRedirect.NewUrlFormat,
+                                      RegexRedirect.OrderNumber);
+        return RedirectToPage();
+    }
+
+    private void Load()
+    {
+        var items = FindRedirects();
+        Message = $"There are currently stored {items.Count()} Regex redirects.";
+        Items = items;
+        RegexRedirect = new RegexRedirectModel { OrderNumber = items.Select(x => x.OrderNumber).Max() + 1 };
+    }
+
+    private IList<RegexRedirect> FindRedirects()
+    {
+        return _redirectLoader.GetAll().ToList();
+    }
+
+    public bool IsEditing(Guid? id)
+    {
+        return id.ToString() == EditItemId;
+    }
+
+    public bool IsEditing()
+    {
+        return !string.IsNullOrEmpty(EditItemId);
     }
 }
