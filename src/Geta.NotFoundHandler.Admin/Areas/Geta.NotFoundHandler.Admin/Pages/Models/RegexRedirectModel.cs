43eb6af7c3d72fbbb1b4494f60721933ae2386a3
Māris Krivtežs <marisks@gmail.com>
2022-09-23 11:46:08 +0300

    Added copyright header.

--- src/Geta.NotFoundHandler.Admin/Areas/Geta.NotFoundHandler.Admin/Pages/Models/RegexRedirectModel.cs
+++ src/Geta.NotFoundHandler.Admin/Areas/Geta.NotFoundHandler.Admin/Pages/Models/RegexRedirectModel.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿// Copyright (c) Geta Digital. All rights reserved.
+// Licensed under Apache-2.0. See the LICENSE file in the project root for more information
+
+using System;
 using System.ComponentModel.DataAnnotations;
-using System.Text.RegularExpressions;
 
 namespace Geta.NotFoundHandler.Admin.Pages.Geta.NotFoundHandler.Admin.Models;
 
--- src/Geta.NotFoundHandler/Data/IRegexRedirectOrderUpdater.cs
+++ src/Geta.NotFoundHandler/Data/IRegexRedirectOrderUpdater.cs
@@ -1,4 +1,7 @@
-﻿namespace Geta.NotFoundHandler.Data;
+﻿// Copyright (c) Geta Digital. All rights reserved.
+// Licensed under Apache-2.0. See the LICENSE file in the project root for more information
+
+namespace Geta.NotFoundHandler.Data;
 
 public interface IRegexRedirectOrderUpdater
 {
